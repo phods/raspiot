1a6c585cd52da4ada0a451947408fabadfcad92f
A K <andregk@gmail.com>
2020-11-05 00:13:02 +0000

    disable pihole and telegraf

--- IOT/services/flask/app/views.py
+++ IOT/services/flask/app/views.py
@@ -8,6 +8,6 @@ def index():
     app_name = os.getenv("APP_NAME")
 
     if app_name:
-        return f"Hello from {app_name} running in a Docker container behind Nginx!"
+        return f"Hellooo from {app_name} running in a Docker container behind Nginx!"
 
     return "Hello from Flask"
